c51dfc7b37c8a217bdbea8fa31686c1c0cac46d3
Alexander Shevtsov <114658340+Incarnadine-WP@users.noreply.github.com>
2023-01-13 18:14:38 +0300

    Cinema and Currency exchange
    Console App

---
+++ Currency exchange/Cinema.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    class CinemaDemo
+    {
+        static void Main(string[] args)
+        {
+            ColorTheme color = new ColorTheme();
+
+            int[] sectors = { 6, 15, 15, 12 };
+            bool usingProgram = true;
+
+            string[,] space = new string[,]
+              {
+                {"Ряд -  1 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  2 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  3 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  4 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  5 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  6 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  7 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  8 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд -  9 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд - 11 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд - 12 места: ","1","2","3","4","5","6","7","8","9","10" },
+                {"Ряд - 13 места: ","1","2","3","4","5","6","7","8","9","10" },
+
+              };
+
+            while (usingProgram)
+            {
+                Console.WriteLine("Добро пожаловать в Кинотеатр.");
+
+                Console.SetCursorPosition(0, 18);
+
+                for (int i = 0; i < space.GetLength(0); i++)
+                {
+                    for (int j = 0; j < space.GetLength(1); j++)
+                    {
+                        if (space[i, j] == "X")
+                            color.ChangeColor(space[i, j] + " ");
+
+                        else
+                            Console.Write(space[i, j] + "  ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("\n\n1) Забронировать места.\n\n2) Выйти из программы.\n");
+                Console.Write("Ваш выбор: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("В каком ряду хотите забронировать место?");
+                        string userSector = Console.ReadLine();
+                        int cleanSector;
+
+                        while (!int.TryParse(userSector, out cleanSector))
+                        {
+                            Console.WriteLine("Выберите доступный вариант программы: 1 или 2");
+                            userSector = Console.ReadLine();
+                        }
+
+                        if (cleanSector > space.GetLength(1) +2 || cleanSector <= 0)
+                        {
+                            Console.WriteLine("Такого ряда не существует.");
+                            break;
+                        }
+
+                        Console.Write("Какое место хотите забронировать?: ");
+                        string placeCount = Console.ReadLine();
+                        int cleanCount;
+
+                        while (!int.TryParse(placeCount, out cleanCount))
+                        {
+                            Console.WriteLine("Это места забронировать невозможно");
+                            placeCount = Console.ReadLine();
+                        }
+
+                        if (cleanCount >= space.GetLength(0) || cleanCount <= 0)
+                        {
+                            Console.WriteLine("Такого места не существует.");
+                            break;
+                        }
+
+                        if (space[cleanSector - 2, cleanCount] != "X")
+                        {
+
+                            Console.WriteLine($"Вы забронировали билет: ряд {cleanSector} место {cleanCount} ");
+                            space[cleanSector - 2, cleanCount] = "X";
+                            Console.WriteLine("\nДля продолжения нажмите любую клавишу.");
+                            break;
+                        }
+
+                        else
+                            Console.WriteLine("Место занято");
+                        break;
+
+                    case "2":
+                        usingProgram = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Выберите доступный вариант 1 или 2");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+
+    class ColorTheme
+    {
+        public void ChangeColor(string item, ConsoleColor color = ConsoleColor.Blue)
+        {
+            ConsoleColor mainColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(item+" ");
+            Console.ForegroundColor = mainColor;
+        }
+    }
+
+}
+
+
---
+++ Currency exchange/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_exchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float USD;
+            float Rub;
+
+            int sellUSD = 66;
+            int buyUSD = 71;
+
+            string userInput;
+
+            string howMuchToChange;
+
+            Console.WriteLine("Добро пожаловать в обмен валют.\nТекущий курс обмена: купить доллары по 71 / продать доллары по 66");
+            Console.WriteLine("Укажите сумму денег на вашем счете.");
+
+            Console.Write("Сколько долларов: ");
+            USD = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Сколько рублей: ");
+            Rub = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию:");
+            Console.WriteLine("1 - обменять рубли на доллары:");
+            Console.WriteLine("2 - обменять доллары на рубли:");
+
+            userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+
+                    Console.Write("Сколько рублей хотите обменять: ");
+                    howMuchToChange = Console.ReadLine();
+                    if (float.TryParse(howMuchToChange, out float result))
+                    {
+                        if (result <= Rub)
+                        {
+                            Rub -= result;
+                            USD += result / buyUSD;
+                        }
+                    }
+
+                    else
+                        Console.WriteLine("невозможно обменять указанную сумму.");
+                    break;
+
+                case "2":
+
+                    Console.Write("Сколько долларов хотите обменять: ");
+                    howMuchToChange = Console.ReadLine();
+                    if (float.TryParse(howMuchToChange, out result))
+                    {
+                        if (result <= USD)
+                        {
+                            Rub += result * sellUSD;
+                            USD -= result;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("невозможно обменять указанную сумму.");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Неверная команда");
+                    break;
+            }
+
+            Console.WriteLine($"Ваш баланс {USD} долларов, {Rub} рублей");
+
+        }
+    }
+}
